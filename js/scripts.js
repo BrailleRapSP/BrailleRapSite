a8f780a229fddf88780048bbbdfca72f62f6de5f
Team BrailleRap-SP  Philippe Pacotte  Stéphane Godin <37216855+BrailleRapSP@users.noreply.github.com>
2020-02-24 16:40:40 +0100

    update mail gateway

--- js/scripts.js
+++ js/scripts.js
@@ -248,7 +248,7 @@
 		var email = encodeURIComponent ($("#cemail").val());
         var message = encodeURIComponent ($("#cmessage").val());
 		
-		var url= "http://127.0.0.1:8000/braillecontact/";
+		var url= "http://marsu.sgodin.com:8000/braillecontact/";
         var data= "name=" + name + "&email=" + email + "&message=" + message; 
 		
 		var xmlhttp = new XMLHttpRequest();
